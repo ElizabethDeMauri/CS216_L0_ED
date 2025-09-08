543736d312b24c38a71e5b877bfaaf9949e07ff7
ElizabethDeMauri <demaurel6093@student.laccd.edu>
2025-09-07 22:29:30 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// Elizabeth DeMauri
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+        cout << "\nMy name is "
+        "Elizabeth DeMauri" << endl;
+
+    cout << "\nEnter the first number ";
+    cin >> num1;
+
+    cout << "\nEnter the second number ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+
+
+My name is Elizabeth DeMauri
+
+Enter the first number 3
+
+Enter the second number 5
+
+The sum of 3 and 5 is 8
+
+*/
